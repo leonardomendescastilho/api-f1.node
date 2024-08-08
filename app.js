f97be4a26e8b32909b0563d64abfd03048358b10
Leonardo de Castilho <leonardo.smendescastilho@gmail.com>
2024-08-08 18:20:46 -0300

    add new

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-import express, { json } from 'express';
-import drivers from './data.js';
+import express from 'express';
+import { drivers } from './data.js';
+import { randomUUID } from 'node:crypto';
 
 const app = express();
+app.use(express.json());
+
 const basePath = '/api/v1';
 
 app.get('/', (req, res) => {
@@ -9,8 +12,40 @@ app.get('/', (req, res) => {
 });
 
 app.get(basePath + '/drivers', (req, res) => {
-  res.status(200).send(drivers);
+	res.status(200).send(drivers);
 });
 
+app.get(basePath + '/drivers/standings/:position', (req, res) => {
+	const { position } = req.params;
+	const selectedDriver = drivers[position - 1];
+	res.status(200).send(selectedDriver);
+});
+
+app.get(basePath + '/drivers/:id', (req, res) => {
+	const { id } = req.params;
+
+	const selectedDriver = drivers.find((driver) => {
+		return driver.id === id;
+	});
+
+	res.status(200).send(selectedDriver);
+});
+
+app.post(basePath + '/drivers', (req, res) => {
+	const newDriver = { ...req.body, id: randomUUID() };
+	drivers.push(newDriver);
+	drivers.sort((a, b) => {
+		if (a.points > b.points) {
+			return -1;
+		}
+	
+		if (b.points > a.points) {
+			return 1;
+		}
+	
+		return 0;
+	});
+	res.status(200).send(newDriver);
+});
 const PORT = 3030;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- data.js
+++ data.js
@@ -127,4 +127,14 @@ const driversInRandomOrder = [
 	},
 ];
 
-export default driversInRandomOrder;
+export const drivers = driversInRandomOrder.sort((a, b) => {
+	if (a.points > b.points) {
+		return -1;
+	}
+
+	if (b.points > a.points) {
+		return 1;
+	}
+
+	return 0;
+});
